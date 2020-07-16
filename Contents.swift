78c8cf9f8c57ddb5beb08bedb0858a4eb4588784
sakshi.jaiswal <sakshi.jaiswal@actolap.com>
2020-07-16 16:23:22 +0530

    first commit

---
+++ Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//MARK: Observables
+
+/* The Observable<T> class allows one or more observers to react to any events in real time and update the app UI, or otherwise process and utilize new and incoming data.*/
